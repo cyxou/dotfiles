0b798beb9e6456e87eb848d33aaaeb23fc5ba096
cyxou <acyxou@gmail.com>
2017-05-01 22:37:41 +0300

    Remove `gb` alias, add `gpt` alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -12,6 +12,7 @@ alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gloga="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative --all"
 alias gp='git push origin HEAD'
+alias gpt='git push --tags origin'
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
@@ -19,5 +20,4 @@ alias gco='git checkout'
 alias gcm='git checkout master'
 alias gcr='git checkout release'
 # alias gcb='git copy-branch-name'
-alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
